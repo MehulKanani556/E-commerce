bb62a299dbce1b1cd2f03cf548d9b44fd25a213b
MehulKanani556 <mkanani556@gmail.com>
2024-11-11 17:48:50 +0530

    product = create , get single, get all

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const app = express();
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 const authRouter = require('./routes/authRoute');
+const productRouter = require('./routes/productRoute');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errorHandler, notFound } = require('./middlewares/errorHandler');
-// const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+
 dbConnect();
+
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -17,6 +21,7 @@ app.use(cookieParser());
 
 
 app.use('/api/user', authRouter);
+app.use('/api/product', productRouter);
 
 
 app.use(notFound);
