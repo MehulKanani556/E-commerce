a8af3f87e5c36a29190e80562ab22f72c919a86c
MehulKanani556 <mkanani556@gmail.com>
2024-11-12 17:27:44 +0530

    update and delete product also filtering product

--- controller/productController.js
+++ controller/productController.js
@@ -3,7 +3,7 @@ const asyncHandler = require('express-async-handler');
 const slugify = require('slugify');
 const createProduct = asyncHandler(async (req, res) => {
     try {
-        if(req.body.title){
+        if (req.body.title) {
             req.body.slug = slugify(req.body.title, { lower: true });
         }
         const newProduct = await Product.create(req.body)
@@ -13,6 +13,31 @@ const createProduct = asyncHandler(async (req, res) => {
     }
 
 });
+
+
+const updateProduct = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        if (req.body.title) {
+            req.body.slug = slugify(req.body.title, { lower: true });
+        }
+
+        const update = await Product.findByIdAndUpdate(id, req.body, { new: true });
+        res.json(update);
+    } catch (error) {
+        throw new Error(error);
+    }
+})
+
+const deleteProduct = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const del = await Product.findByIdAndDelete(id);
+        res.json(del);
+    } catch (error) {
+        throw new Error(error);
+    }
+})
 
 
 const getaProduct = asyncHandler(async (req, res) => {
@@ -30,7 +55,46 @@ const getaProduct = asyncHandler(async (req, res) => {
 
 const getAllProduct = asyncHandler(async (req, res) => {
     try {
-        const products = await Product.find();
+
+        // Filtering
+        const queryObject = { ...req.query };
+        const excludeFields = ['page', 'sort', 'limit', 'fields'];
+        const obj = excludeFields.forEach((el) => delete queryObject[el]);
+
+        let queryStr = JSON.stringify(queryObject);
+        queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, match => `$${match}`);
+        let qry = Product.find(JSON.parse(queryStr));
+
+        // Sorting
+
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            qry = qry.sort(sortBy);
+        } else {
+            qry = qry.sort('-createdAt');
+        }
+
+        // Limiting the field
+
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            qry = qry.select(fields);
+        } else {
+            qry = qry.select('-__v');
+        }
+
+        // Pagination
+
+        const page =req.query.page;
+        const limit =req.query.limit;
+        const skip = (page - 1) * limit;
+        qry = qry.skip(skip).limit(limit);
+        if(req.query.page){
+            const totalCount = await Product.countDocuments();
+            if(skip>= totalCount) throw new Error('This page does not exists');
+        }
+
+        const products = await qry;
         res.json(products);
     } catch (error) {
         throw new Error(error);
@@ -38,4 +102,4 @@ const getAllProduct = asyncHandler(async (req, res) => {
 });
 
 
-module.exports = { createProduct, getaProduct, getAllProduct };
+module.exports = { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { createProduct, getaProduct, getAllProduct } = require('../controller/productController');
+const { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct } = require('../controller/productController');
 const router = express.Router();
-
-router.post('/',createProduct);
-router.get('/:id',getaProduct);
-router.get('/',getAllProduct);
+const { isAdmin, authMiddleware } = require('../middlewares/authMiddleware');
+router.post('/', authMiddleware, isAdmin, createProduct);
+router.get('/', getAllProduct);
+router.get('/:id', getaProduct);
+router.put('/:id', authMiddleware, isAdmin, updateProduct);
+router.delete('/:id', authMiddleware, isAdmin, deleteProduct);
 
 module.exports = router;
