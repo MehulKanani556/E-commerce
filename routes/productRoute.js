f1368c8335e5728f07d2ef6d301cc65d6daaa88a
Mehul Kanani <mkanani556@gmail.com>
2024-11-21 23:54:58 +0530

    implement multer and cloudinary

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,11 +2,13 @@ const express = require('express');
 const { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct, addToWishlist, rating } = require('../controller/productController');
 const router = express.Router();
 const { isAdmin, authMiddleware } = require('../middlewares/authMiddleware');
+const { uploadPhoto,productImageResize } = require('../middlewares/uploadImages');
 
 
 router.post('/', authMiddleware, isAdmin, createProduct);
 router.get('/', getAllProduct);
 router.get('/:id', getaProduct);
+router.put('/upload/:id',authMiddleware,isAdmin,uploadPhoto.array('images',10),productImageResize)
 router.put('/wishlist', authMiddleware,  addToWishlist);
 router.put('/rating', authMiddleware,  rating);
 router.put('/:id', authMiddleware, isAdmin, updateProduct);
